0cc15bf49651b60f81f320020047e9e228cf207f
Harry Vicaradge <harryvic_@gmail.com>
2026-03-16 15:41:45 +0000

    tweaked base testing values

--- job_cpu.sh
+++ job_cpu.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=fluid_sim_bench
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32             
+#SBATCH --cpus-per-task=128            
 #SBATCH --time=02:00:00               
 #SBATCH --output=logs/%j_out.txt
 #SBATCH --error=logs/%j_err.txt
@@ -15,10 +15,10 @@ export OMP_PROC_BIND=close
 
 WIDTH=500
 HEIGHT=500
-INITIAL_U_X=0.075
+INITIAL_U_X=0.015
 TAU=0.55
-MAX_STEPS=10000
-SAVE_FREQ=50
+MAX_STEPS=50000
+SAVE_FREQ=250
 
 echo "Starting simulation"
 ./lbm_sim_cpu $WIDTH $HEIGHT $INITIAL_U_X $TAU $MAX_STEPS $SAVE_FREQ
