1a56ac7f618686067d5f3423a1845719ac3f321d
Harry Vicaradge <harryvic_@gmail.com>
2026-03-19 23:51:32 +0000

    fixed dumb python not running error

--- jobs/job_cpu.sh
+++ jobs/job_cpu.sh
@@ -27,7 +27,7 @@ echo "Starting simulation"
 echo "Simulation complete, Rendering video..."
 
 source venv/bin/activate
-python plot.py $WIDTH $HEIGHT $INITIAL_U_X $SLURM_JOB_ID
+python3 plot.py $WIDTH $HEIGHT $INITIAL_U_X $SLURM_JOB_ID
 
 echo "Render finished"
 
